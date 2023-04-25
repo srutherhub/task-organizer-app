d3c9e61cee65c7b035b748d58302726d3686cb27
srutherhub <samuelbrutherford@gmail.com>
2023-04-25 16:45:55 -0700

    Started to add firebase database

---
+++ src/config/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCli9WaHX9dwJLs-b1mr4U0SVpnlvZmUmI",
+  authDomain: "productivityapp-53aaa.firebaseapp.com",
+  projectId: "productivityapp-53aaa",
+  storageBucket: "productivityapp-53aaa.appspot.com",
+  messagingSenderId: "310668799414",
+  appId: "1:310668799414:web:8f4714827897bddc8e6517",
+  measurementId: "G-QVB5GESRSG",
+};
+
+const app = initializeApp(firebaseConfig);
